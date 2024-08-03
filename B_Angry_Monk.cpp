f75b6f9c47420c1114f76c2a074e8957ad34b77b
subhamsubham6063 <sadhukhansubham588@gmail.com>
2024-08-03 13:02:36 +0530

    testing

--- B_Angry_Monk.cpp
+++ B_Angry_Monk.cpp
@@ -12,7 +12,7 @@ using namespace std;
 void err(istream_iterator<string> it) {}
 template<typename T, typename... Args>
 void err(istream_iterator<string> it, T a, Args... args) {
-    cerr << *it << " = " << a << endl;
+    // cerr << *it << " = " << a << endl;gia
     err(++it, args...);
 }
 
